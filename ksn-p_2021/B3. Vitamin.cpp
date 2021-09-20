795506fb001b77a56f53917c6070c1665a17ac83
Hanif <hanifabid19@gmail.com>
2021-09-20 13:14:40 +0700

    initial commit

---
+++ ksn-p_2021/B3. Vitamin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int jumlah = 2;
+	int uang = 10;
+	
+	int harga[1000];
+	int kandungan[1000];
+	int dosis[1000];
+	
+	for(int i = 0; i < jumlah ; i++){
+		cin >> harga[i] >> kandungan[i] >> dosis[i];
+	}
+}
+
